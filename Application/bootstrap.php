cff8e0e289ebb49d72179eb17d201a9b38d5fda2
Sergey Savelyev <serg_savel@mail.ru>
2024-02-29 07:06:28 +0500

    mvc pattern

--- Application/bootstrap.php
+++ Application/bootstrap.php
@@ -6,8 +6,6 @@ require_once 'core/view.php';
 require_once 'core/controller.php';
 require_once 'core/route.php'; 
 
-phpinfo();
-
 // Вызов статической функции для начала загрузки всей страницы
 Route::start();
 
