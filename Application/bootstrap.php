914e93313722f6b02d8d5d3b191255d0ecdf1762
Sergey Savelyev <serg_savel@mail.ru>
2024-03-05 19:05:42 +0500

    fix paths

--- Application/bootstrap.php
+++ Application/bootstrap.php
@@ -1,6 +1,8 @@
 <?php
-// Подключение файлов
+// Подключение файла с путями к нужным папкам
 require_once 'Application/Config/paths.php';
+
+// Подключение файлов
 require_once CORE . '/model.php'; 
 require_once CORE . '/view.php'; 
 require_once CORE . '/controller.php';
